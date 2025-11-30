8d49ded8330aa6efcae4f414df6bc426fbeee410
Jason <66013853+ahmola@users.noreply.github.com>
2025-11-30 18:37:37 +0900

    gateway and config #8

--- backend/edge/gateway/src/test/java/dev/ahmola/photogram/gateway/GatewayApplicationTests.java
+++ backend/edge/gateway/src/test/java/dev/ahmola/photogram/gateway/GatewayApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 class GatewayApplicationTests {
 
 	@Test
-	void contextLoads() { // something
+	void contextLoads() { // something12
 	}
 
 }
